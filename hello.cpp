b425473bb2a238d8019591deef71aa8e0a116086
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2023-04-05 21:47:08 +0530

    02

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
-    cout<<3<<endl;
+
+    vector<int>v={1,2,3,4,5};
+    // v.push_back(10);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i];
+    }
+    return 0;
 }
